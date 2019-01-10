373bdec607ecc7805bce1b9e1c28a11624ec3584
bamss <amadougolfa98@gmail.com>
2019-01-10 13:47:39 +0100

    exercice 00_hello.rb fait

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,4 +1,16 @@
+def ask_first_name
+    puts "salut, c'est quoi ton blase ?"
+    print "> "
+    first_name = gets.chomp
+    return  first_name
+end
 def say_hello(first_name)
     puts "bonjour, #{first_name}"
 end
-say_hello("Amadou")
+
+def perfom 
+   first_name = ask_first_name
+    say_hello(first_name)
+end
+
+perfom

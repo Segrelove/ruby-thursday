7f084af4925a6d30110abe86206d9a2e4ddb9956
Segrelove <mlesegret@gmail.com>
2019-01-10 16:59:19 +0100

    password methode numero 2

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,25 +1,36 @@
 def signup
     puts "Choisis un mot de passe"
     print "> "
-    password = gets.chomp.to_s
+    password = gets.chomp
     return password
 end
 
-def login(verification)
-    puts "Quel est ton mot de passe?"
-    print "> "
-    verification = gets.chomp.to_s
-    return verification
+def login
+    y = signup
+        puts "Quel est ton mot de passe?"
+        print "> "
+        x = gets.chomp
 
-    if verification == password
-        puts "Bien ouej"
-    else
-        puts "Error"
-    end
+        if x == y
+            puts "Bien ouej"
+            return success
+        else
+            puts "Mauvais Mot de Passe"
+            return failure
+        end
 end
 
+# def welcome_screen
+#     x = login
+
+#     if x == true 
+#         puts "Bienvenue"
+#     else
+#         puts "BARRE TOI"
+#     end
+# end
+
 def perform
-    signup
     login
 end
 

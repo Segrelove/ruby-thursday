a315340b0959af6de6b41ec55c8d6cd662426190
Segrelove <mlesegret@gmail.com>
2019-01-10 14:52:14 +0100

    ajout method qui bannie les nombres pairs

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,14 +1,49 @@
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-print "> "
-nb = gets.chomp.to_i
-i = 1
-if nb >= 1 && nb <= 25
-    while nb > i
-        
-        print " " * (nb - i)
-        i += 1
-        puts "#" * (i - 1) + "#" * (i - 2)
+def half_pyramid
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+    print "> "
+    nb = gets.chomp.to_i
+    i = 1
+    if nb >= 1 && nb <= 25
+        while nb > i
+            print " " * (nb - i)
+            i += 1
+            puts "#" * (i - 1)
+        end
+    else
+        "fais Belek a toi 😡"
     end
-else
-    "fais Belek a toi 😡"
 end
+
+def full_pyramid
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+    print "> "
+    nb = gets.chomp.to_i
+    i = 1
+    if nb >= 1 && nb <= 25
+        while nb > i
+            print " " * (nb - i)
+            i += 1
+            puts "#" * (i - 1) + "#" * (i - 2)
+        end
+    else
+        "fais Belek a toi 😡"
+    end
+end
+
+def reverse_pyramid
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+    print "> "
+    nb = gets.chomp.to_i
+    i = 1
+    if nb >= 1 && nb <= 25 && nb % 2 != 0
+        while nb > i
+            print " " * (nb - i)
+            i += 1
+            puts "#" * (i - 1) + "#" * (i - 2)
+        end
+    else
+        puts "fais Belek a toi 😡"
+    end
+end
+
+reverse_pyramid
